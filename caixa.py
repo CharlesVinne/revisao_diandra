d3ed89a39b3a64648d4478d2197b03ea4b75d2eb
unknown <vinnesousa22@gmail.com>
2020-01-24 16:44:25 -0300

    Cadastro caixa eletronico,saldo,saque,deposito

--- caixa.py
+++ caixa.py
@@ -4,3 +4,40 @@ from classes import Cliente, Conta
 
 class CaixaEletronico():
   def __init__(self):
+    nome = input('Qual o seu Nome ?')
+    cpf = input('Digite seu cpf: ')
+    cliente = Cliente(nome, cpf)
+    self.conta = Conta(cliente)
+
+    print(f"Olá {self.conta.titular.nome}, sua conta é: {self.conta.numero}")
+
+
+  def exibir_menu(self):
+
+    print("1- Consultar saldo\n 2- Depositar\n 3- Sacar")
+  
+    opcao = input('Digite uma opção: ')
+
+    if opcao == '1':
+      valor = self.conta.consultar_saldo()
+      print (f'Seu saldo é de {valor}')
+
+    elif opcao == '2':
+      valor = float(input('Valor do deposito: '))
+      self.conta.deposito(valor)
+      print(f'Depósito efetuado de {valor}')
+
+    elif opcao == '3':
+      valor = float(input('Valor do saque: '))
+
+      if self.conta.sacar(valor):
+        print('saque efetuado')
+
+      else:
+        print('não tem saldo')
+
+    # else:
+    #   print('Opção invalida')
+
+
+
--- principal.py
+++ principal.py
@@ -1,1 +1,16 @@
+
+from caixa import CaixaEletronico
 # arquivo que vai rodar no terminal
+
+caixaeletronico = CaixaEletronico()
+
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
+caixaeletronico.exibir_menu()
